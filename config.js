5a3615d198ea8d9ecae5da50a3ed9dc7ea17849d
LUIS ALEXANDER PILATUÑA LOPEZ <luis.pilatuna.lopez@estudiantes.cordillera.edu.ec>
2023-05-30 16:03:49 -0500

    add config

--- config.js
+++ config.js
@@ -3,3 +3,8 @@ import { config } from "dotenv";
 config();
 
 export const Secret_key = process.env.Secret_key;
+export const HOST = process.env.HOST;
+export const PORT = process.env.PORT;
+export const USER = process.env.USER;
+export const PASSWORD = process.env.PASSWORD;
+export const DATABASE = process.env.DATABASE;
