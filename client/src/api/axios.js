723b28134421ac9e45bdd0d95680320b27d42414
LUIS ALEXANDER PILATUÑA LOPEZ <luis.pilatuna.lopez@estudiantes.cordillera.edu.ec>
2023-06-19 16:40:29 -0500

    add comment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 //configuracion especial para establecer la cookie en el frontend
 const instance = axios.create({
   baseURL: "http://localhost:4001/api",
-  withCredentials: true,
+  withCredentials: true, //permite enviar las cookies al frontend
 });
 export default instance;
