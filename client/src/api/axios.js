4dc1b8db029db155eda7f51a309a5aa7fc49d52e
LUIS ALEXANDER PILATUÑA LOPEZ <luis.pilatuna.lopez@estudiantes.cordillera.edu.ec>
2024-01-07 13:19:23 -0500

    add backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 //configuracion especial para establecer la cookie en el frontend
+const URL = import.meta.env.BACKEND_URL;
+
 const instance = axios.create({
-  baseURL: "https://appnotes-17jx.onrender.com/api",
+  baseURL: `${URL}/api` || "http://localhost:4001/api",
   withCredentials: true, //permite enviar las cookies al frontend
 });
 export default instance;
